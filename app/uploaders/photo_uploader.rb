670842f314ea6ec5589e13b8ee1f34a8ed79d60b
jeandgardany <jeandgardany@hotmail.com>
2019-10-29 19:59:51 +0000

    Correções nas Fotos ImageMagick

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -33,11 +33,11 @@ class PhotoUploader < CarrierWave::Uploader::Base
   #   process resize_to_fit: [50, 50]
   # end
    version :thumb do
-     process resize_to_fill: [400, 400]
+     process resize_to_fill: [50, 50]
    end
 
    version :small_thumb, from_version: :thumb do
-     process resize_to_fill: [50, 50]
+     process resize_to_fill: [300, 300]
    end
 
   # Add a white list of extensions which are allowed to be uploaded.
@@ -54,9 +54,9 @@ class PhotoUploader < CarrierWave::Uploader::Base
   
   #include CarrierWave::MiniMagick
 
-  process resize_to_fit: [200, 200]
+  process resize_to_fit: [300, 300]
 
-  version :thumb do
-    process resize_to_fill: [50,50]
-  end
+  #version :thumb do
+  #  process resize_to_fill: [50,50]
+  #end
 end
