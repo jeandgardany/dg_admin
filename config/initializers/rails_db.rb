edb578f5a0504dc5c5ec24f152beb33bacc2731d
jeandgardany <jeandgardany@hotmail.com>
2020-04-28 08:10:01 -0700

    Adcionado autenticação no Admin e no DB

--- config/initializers/rails_db.rb
+++ config/initializers/rails_db.rb
@@ -13,15 +13,16 @@ if Object.const_defined?('RailsDb')
     # config.white_list_tables = ['posts', 'comments']
 
     # # Enable http basic authentication
-    # config.http_basic_authentication_enabled = false
+    config.http_basic_authentication_enabled = true
 
     # # Enable http basic authentication
-    # config.http_basic_authentication_user_name = 'rails_db'
+    config.http_basic_authentication_user_name = 'dggirl'
 
     # # Enable http basic authentication
-    # config.http_basic_authentication_password = 'password'
+    config.http_basic_authentication_password = '090271jd'
 
     # # Enable http basic authentication
-    # config.verify_access_proc = proc { |controller| true }
+    config.verify_access_proc = proc { |controller| true }
   end
 end
+
