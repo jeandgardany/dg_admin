4c95015c6b293008d51cac332bba83d7b6453195
jeandgardany <jeandgardany@hotmail.com>
2020-04-29 12:44:53 -0700

    Rotas ocultas

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,11 @@ Rails.application.routes.draw do
   root :to => 'products#inputs'
   resources :contact, only: :index
   resources :about, only: :index
+
+   #get '404', :to => 'application#page_not_found'
+   match '/404', to: 'errors#not_found', :via => :all
+   match '/422', to: 'errors#unacceptable', :via => :all
+   match '/500', to: 'errors#internal_error', :via => :all
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
